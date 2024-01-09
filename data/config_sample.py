60f7b6e3a0d016f952e5aedaf6fee3d5b75521b3
CmmSheparD <ustynow.gleb@gmail.com>
2024-01-10 00:03:28 +0300

    chore: Sample config file.

---
+++ data/config_sample.py
@@ -0,0 +1,10 @@
+"""Sample config that should be placed in the 'gameratebot' package.
+Remember: Never commit any config data into any public repositories.
+"""
+
+token = ''
+
+db_socket = ''
+db_name = ''
+db_user = ''
+db_password = ''
